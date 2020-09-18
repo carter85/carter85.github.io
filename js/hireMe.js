7f2fc40a30adf3f15046b7ee09bdcc13e6edb5a6
Casey Carter <caseycarter8520@gmail.com>
2020-09-17 21:53:20 -0500

    removed unused JS files

--- js/hireMe.js
+++ js/hireMe.js
@@ -1,17 +1,3 @@
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 50) {
-      document.getElementById("navbar").style.top = "0";
-} else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-}
-
-
-    
-  
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
